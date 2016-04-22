52db0a32b3eddf03317955b6bcc03e6ca836cdb6
brunobrandes <brunobrandes.as@gmail.com>
2016-04-21 21:56:24 -0300

    Create a C# example project using the new Bot Application template

---
+++ example/Bot/Api.Ai.Example.Bot.Application/App_Start/BotAppBootstrap.cs
@@ -0,0 +1,32 @@
+﻿using Api.Ai.ApplicationService.Factories;
+using Api.Ai.Domain.Service.Factories;
+using Api.Ai.Infrastructure.Factories;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Api.Ai.Example.Bot.Application
+{
+    public class BotAppBootstrap
+    {
+        public static Container RegisterContainer(HttpConfiguration config)
+        {
+            var container = new Container();
+
+            container.RegisterWebApiControllers(config);
+            
+            container.RegisterSingleton<IServiceProvider>(container);
+
+            container.RegisterSingleton<IApiAiAppServiceFactory, ApiAiAppServiceFactory>();
+            container.RegisterSingleton<IHttpClientFactory, HttpClientFactory>();
+
+            config.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+
+            return container;
+        }
+    }
+}
---
+++ example/Bot/Api.Ai.Example.Bot.Application/Controllers/MessagesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Utilities;
+using Newtonsoft.Json;
+using Api.Ai.ApplicationService.Factories;
+using Api.Ai.Domain.Service.Factories;
+using Api.Ai.Domain.DataTransferObject.Request;
+
+namespace Api.Ai.Example.Bot.Application
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        #region Private Fields
+
+        private readonly IApiAiAppServiceFactory _apiAiAppServiceFactory;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        #endregion
+
+        #region Constructor
+
+        public MessagesController(IApiAiAppServiceFactory apiAiAppServiceFactory, IHttpClientFactory httpClientFactory)
+        {
+            _apiAiAppServiceFactory = apiAiAppServiceFactory;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Message HandleSystemMessage(Message message)
+        {
+            if (message.Type == "Ping")
+            {
+                Message reply = message.CreateReplyMessage();
+                reply.Type = "Ping";
+                return reply;
+            }
+            else if (message.Type == "DeleteUserData")
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == "BotAddedToConversation")
+            {
+            }
+            else if (message.Type == "BotRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "UserAddedToConversation")
+            {
+            }
+            else if (message.Type == "UserRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "EndOfConversation")
+            {
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Call query api.ai
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private async Task<string> GetSpeechAsync(Message message)
+        {
+            var queryAppService = _apiAiAppServiceFactory.CreateQueryAppService("https://api.api.ai/v1", "YOUR_ACCESS_TOKEN");
+
+            var queryRequest = new QueryRequest
+            {
+                Query = new string[] { message.Text },
+                Lang = Domain.Enum.Language.English,
+                SessionId = message.Id
+            };
+
+            var queryResponse = await queryAppService.GetQueryAsync(queryRequest);
+
+            if (queryResponse != null && queryResponse.Result != null)
+            {
+                return queryResponse.Result.Fulfillment.Speech;
+            }
+            else
+            {
+                return "Oops! You can try to repeat that, please?";
+            }
+        }
+
+        #endregion
+
+        #region Api Methods
+
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<Message> Post([FromBody]Message message)
+        {
+            if (message.Type == "Message")
+            {
+                var speech = await GetSpeechAsync(message);
+                
+                // return our reply to the user
+                return message.CreateReplyMessage(speech);
+            }
+            else
+            {
+                return HandleSystemMessage(message);
+            }
+        }
+
+        #endregion
+    }
+}
